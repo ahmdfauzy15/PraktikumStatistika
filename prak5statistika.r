b25354d7df9514b091fe730cd14ea3141d8c5480
Achmad Fauzy <91053494+ahmdfauzy15@users.noreply.github.com>
2022-10-15 15:05:13 +0700

    Create prak5statistika.r

---
+++ prak5statistika.r
@@ -0,0 +1,11 @@
+amad=read.delim("clipboard")
+View(amad)
+str(amad)
+mean(amad$volume)
+t.test(amad$volume,mu=10)
+
+amad=read.delim("clipboard")
+View(amad)
+str(amad)
+mean(amad$pendapatan)
+t.test(amad$pendapatan,mu=14500)
